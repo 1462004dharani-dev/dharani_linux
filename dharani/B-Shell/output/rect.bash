37a1c5bf819e65e94e98fea01897c03a182340ef
1462004dharani-dev <1462004dharani@gmail.com>
2026-03-25 12:35:22 +0530

    Add files via upload

---
+++ dharani/B-Shell/output/rect.bash
@@ -0,0 +1,15 @@
+
+echo "Enter the length:"
+read length
+
+echo "Enter the width:"
+read width
+
+
+area=$((length * width))
+
+perimeter=$((2 * (length + width)))
+
+
+echo "Area of rectangle: $area"
+echo "Perimeter of rectangle: $perimeter"
